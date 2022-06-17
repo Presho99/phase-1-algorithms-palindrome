66540505b605087ff1296ac846f0f56e4711088d
Precious <Preshomondi@gmail.com>
2022-06-17 12:28:24 +0300

    commit tree c6d0f18f3e52b86af1d0d21d0c4352639f7b3199

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+    const arrSplit = word.split('')
+    const arrReverse = arrSplit.reverse()
+    const arrStr = arrReverse.join('')
+  
+    if(word == arrStr) {
+      return(true)
+    }
+    else{
+      return(false)
+    }
+  }
+  
+  
+  
+
 
 /* 
   Add your pseudocode here
+
+  If string reads same forwards and backwards
+  return true
+  else 
+  return false
 */
 
 /*
   Add written explanation of your solution here
+  I used .split("") method to convert the string into an array. I reversed the array using .reverse() method. 
+  I then used the .join("") method to convert the reversed array into a string. If the initial string is equal to the reversed string,
+  then the function isPalindrome() will return true, otherwise it will return false.
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +43,8 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  
 }
 
 module.exports = isPalindrome;
